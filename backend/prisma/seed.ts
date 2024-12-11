7dc74e9630cb52bdcb63798a6b3c797650fab863
Oláh András <106300817+oandris228@users.noreply.github.com>
2024-12-11 08:58:12 +0100

    seeding

--- backend/prisma/seed.ts
+++ backend/prisma/seed.ts
@@ -1,1 +1,27 @@
-import { faker } from '@faker-js/faker';
+import { faker } from '@faker-js/faker'
+import {PrismaClient} from '@prisma/client'
+
+const prisma = new PrismaClient
+
+const materials = ["wood", "metal", "plastic", "other"]
+
+async function main() {
+    for (let i = 0; i < 5; i++) {
+        await prisma.product.create({
+            data: {
+                name: faker.commerce.productName(),
+                price: faker.number.int( {min: 1, max: 500}),
+                description: faker.commerce.productDescription()
+            }
+        })
+    }
+}
+main()
+    .then(async ()=> {
+        await prisma.$disconnect();
+    })
+    .catch(async (e)=>{
+        console.log(e)
+        await prisma.$disconnect()
+        process.exit(1)
+    })
